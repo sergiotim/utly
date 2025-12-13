934b1822046cc39acdde3596b99eb72a694921c2
Sérgio Timoteo <74915883+sergiotim@users.noreply.github.com>
2025-12-12 21:30:32 -0300

    feat:prisma configurado

---
+++ lib/prisma.ts
@@ -0,0 +1,27 @@
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import ws from "ws";
+
+dotenv.config();
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL!;
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+function prismaClientSingleton(): PrismaClient {
+  const adapter = new PrismaNeon({
+    connectionString,
+  });
+
+  return new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
